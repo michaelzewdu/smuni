f6edbf589f703db2f6973af9bf2df37c4a7b8d07
slim-crown <michaelzewdu10@gmail.com>
2021-09-10 16:54:27 +0300

    Started localization...

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:smuni/routes.dart';
 import 'package:smuni/screens/home_screen.dart';
 
@@ -13,17 +14,27 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Semuni',
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate
+      ],
+      supportedLocales: [
+        Locale('en',''), //English
+        Locale('am',''), //አማርኛ
+        Locale('ti',''), //ትግርኛ
+        Locale('aa',''), //አፋር
+        Locale('so',''), //ሶማሊ
+        Locale('sgw',''), //ሰባት ቤት ጉራጌ
+        Locale('sid',''), //ሲዳሞ
+        Locale('wal',''), //ወላይታ
+        
+
+
+      ],
       theme: ThemeData(
-          // This is the theme of your application.
-          //
-          // Try running your application with "flutter run". You'll see the
-          // application has a blue toolbar. Then, without quitting the app, try
-          // changing the primarySwatch below to Colors.green and then invoke
-          // "hot reload" (press "r" in the console where you ran "flutter run",
-          // or simply save your changes to "hot reload" in a Flutter IDE).
-          // Notice that the counter didn't reset back to zero; the application
-          // is not restarted.
+
           primarySwatch: primarySmuniSwatch),
       home: SmuniHomeScreen(),
       onGenerateRoute: Routes.myOnGenerateRoute,
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -91,6 +91,7 @@ class SmuniHomeScreen extends StatelessWidget {
                   HorizontalCards(),
                   HorizontalCards(),
                   HorizontalCards(),
+
                 ],
               ),
             ),
